9d46ee99974d571c77a0d4c4ac50d9fe41eb0c54
Michael Lee <micl2e2@proton.me>
2023-07-24 17:29:53 +0800

    change email

--- src/public_macros.rs
+++ src/public_macros.rs
@@ -1,3 +1,14 @@
+// Copyright (C) 2023 Michael Lee <micl2e2@proton.me>
+//
+// Licensed under the MIT License <LICENSE-MIT or
+// https://opensource.org/license/mit> or the GNU General Public License,
+// Version 3.0 or any later version <LICENSE-GPL or
+// https://www.gnu.org/licenses/gpl-3.0.txt>, at your option.
+//
+// This file may not be copied, modified, or distributed except except in
+// compliance with either of the licenses.
+//
+
 #[cfg_attr(doc_cfg, doc(cfg(feature = "use_std")))]
 #[cfg(feature = "use_std")]
 mod stdonly {
